38d162a425f50a9f7593665fa29e2e0542c7149c
shahar calfon <shaharcalfon@gmail.com>
2021-07-01 01:26:45 +0300

    start with the authentication

---
+++ controllers/authController.js
@@ -0,0 +1,20 @@
+//const jwt = require('jsonwebtoken');
+const User = require('./../models/userModel');
+
+exports.signup = async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newUser,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -25,14 +26,25 @@ const userSchema = new mongoose.Schema({
     {
       type: mongoose.Schema.ObjectId,
       ref: 'Run',
+      default: [],
     },
   ],
   groups: [
     {
       type: mongoose.Schema.ObjectId,
       ref: 'Group',
+      default: [],
     },
   ],
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
