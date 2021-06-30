829ef1bf3c24bc480ba6f4a3bf0ca01adee60ab7
shahar calfon <shaharcalfon@gmail.com>
2021-06-30 18:22:02 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
+const groupRouter = require('./routes/groupRouter');
+const groupRunRouter = require('./routes/groupRunRouter');
+const runRouter = require('./routes/runRouter');
+const userRouter = require('./routes/userRouter');
+
 const app = express();
 
 app.use(express.json());
@@ -8,6 +13,11 @@ app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
   next();
 });
+
+app.use('/run-with-me/groups', groupRouter);
+app.use('/run-with-me/groups-run', groupRunRouter);
+app.use('/run-with-me/runs', runRouter);
+app.use('/run-with-me/users', userRouter);
 
 //Handle for unknown URL.
 app.all('*', (req, res, next) => {
