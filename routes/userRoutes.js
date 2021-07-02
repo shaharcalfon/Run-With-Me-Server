48121473eaccfa44f753f1c348c89c7c200d0c67
shahar calfon <shaharcalfon@gmail.com>
2021-07-02 18:01:44 +0300

    auth update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,6 @@ const authController = require('./../controllers/authController');
 const userRouter = express.Router();
 
 userRouter.post('/signup', authController.signup);
+//userRouter.post('/login', authController.login);
 
 module.exports = userRouter;
