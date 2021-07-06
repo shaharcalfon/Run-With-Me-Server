5e01fd42a9c97c3618649449370942ac57f10247
shahar calfon <shaharcalfon@gmail.com>
2021-07-06 14:26:22 +0300

    runData model fix

--- models/routeModel.js
+++ models/routeModel.js
@@ -4,6 +4,6 @@ const routeSchema = new mongoose.Schema({
   coordinates: [[Number]],
 });
 
-const Route = mongoose.model('GroupRunData', routeSchema);
+const Route = mongoose.model('RunRoute', routeSchema);
 
 module.exports = Route;
